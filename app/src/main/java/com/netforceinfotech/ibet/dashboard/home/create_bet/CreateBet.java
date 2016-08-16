596ef8f57f6280a8a54978d14847d7d55b12bf84
kunsang wangyal <kunwangyal05@gmail.com>
2016-08-16 15:29:43 +0530

    minor changes

--- app/src/main/java/com/netforceinfotech/ibet/dashboard/home/create_bet/CreateBet.java
+++ app/src/main/java/com/netforceinfotech/ibet/dashboard/home/create_bet/CreateBet.java
@@ -3,9 +3,13 @@ package com.netforceinfotech.ibet.dashboard.home.create_bet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,7 +21,8 @@ public class CreateBet extends AppCompatActivity {
     Toolbar toolbar;
     ImageView setting_list1, setting_list2, setting_list3;
     private MaterialDialog dailog;
-
+    TextView textViewRemaining;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +34,13 @@ public class CreateBet extends AppCompatActivity {
     }
 
     private void setuplayout() {
+        editText = (EditText) findViewById(R.id.editText);
+        textViewRemaining = (TextView) findViewById(R.id.textViewRemaing);
+        setting_list1 = (ImageView) findViewById(R.id.canViewJoin);
 
-        setting_list1 = (ImageView) findViewById(R.id.setting_list_icon);
+        setting_list2 = (ImageView) findViewById(R.id.canView);
 
-        setting_list2 = (ImageView) findViewById(R.id.setting_list_icon2);
-
-        setting_list3 = (ImageView) findViewById(R.id.setting_list_icon3);
+        setting_list3 = (ImageView) findViewById(R.id.cantView);
 
         boolean wrapInScrollView = true;
 
@@ -59,6 +65,23 @@ public class CreateBet extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showPopUp("If all checkboxes are off. By default all users can view and join the bet 3");
+            }
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+                int remaining = 200 - count;
+                textViewRemaining.setText(remaining + " characters remaining");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
@@ -95,8 +118,7 @@ public class CreateBet extends AppCompatActivity {
 
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
             case android.R.id.home:
